1077297c4b9f14d519b583a72989b75424c13955
ismailkaraca <ismail.krc@gmail.com>
2025-10-11 04:22:26 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class', // Karanlık mod için class stratejisi
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
